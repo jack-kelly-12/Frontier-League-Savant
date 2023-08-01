3124c00aa3dd751ae5c802b977b1f0ea2cf1f7dc
Jack Kelly LBL <jackkelly12902@gmail.com>
2023-08-01 16:25:10 -0500

    Initial commit

---
+++ src/static/main.js
@@ -0,0 +1,49 @@
+function updateDisplayedImages(selectedUmpire) {
+  $('#plotContainer').empty();
+  const imageNames = ['edge_calls.png', 'heat_maps.png', 'total.png']; // Image filenames
+  for (const imageName of imageNames) {
+    const imagePath = `UmpireReports/${selectedUmpire}/${imageName}`;
+    const imgElement = $('<img>').attr('src', imagePath);
+    $('#plotContainer').append(imgElement);
+  }
+}
+
+$(document).ready(function () {
+  // Fetch the list of umpires from the server-side route
+  $.get('/umpires', function (data) {
+    const umpires = data.umpires;
+    const umpireSelect = $('#umpireSelect');
+
+    // Generate options for umpires in the dropdown
+    umpires.forEach((umpire) => {
+      const option = $('<option>').val(umpire).text(umpire);
+      umpireSelect.append(option);
+    });
+
+    // Handle dropdown selection change
+    umpireSelect.change(function () {
+      const selectedUmpire = $(this).val();
+      updateDisplayedImages(selectedUmpire);
+    });
+
+    updateDisplayedImages(umpireSelect.val());
+  });
+});
+
+
+$(document).ready(function () {
+        // Function to update the image when the dropdown selection changes
+        function updateImage() {
+            var selectedPitcher = $("#pitcherSelect").val();
+            var imagePath = "/static/Usages/" + selectedPitcher + ".png";
+            $("#pitcherImage").attr("src", imagePath);
+        }
+
+        // Call the updateImage function initially to set the image based on the first option
+        updateImage();
+
+        // Listen for changes to the dropdown selection
+        $("#pitcherSelect").change(function () {
+            updateImage();
+        });
+    });
